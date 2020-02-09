0e72c94aebd31590f04af719eba89c7f3029ac5a
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-09 17:51:48 -0500

    almost done

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+  attr_accessor :genre
+  def actors_list
+    self.actors.map {|t| t.full_name}
+  end
 end
