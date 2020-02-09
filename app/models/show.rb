fc624ee72f92b98821b9ead4d7bd502f0ab114f4
rjwilson0531 <rjwilson0531@gmail.com>
2020-02-09 18:08:18 -0500

    1 more test

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,6 @@
 class Show < ActiveRecord::Base
   belongs_to :network
+  belongs_to :genre
   has_many :characters
   has_many :actors, through: :characters
   attr_accessor :genre
---
+++ db/migrate/20200209230449_add_genres_to_shows.rb
@@ -0,0 +1,5 @@
+class AddGenresToShows < ActiveRecord::Migration[6.0]
+  def change
+    add_column :shows, :genre_id, :integer
+  end
+end
